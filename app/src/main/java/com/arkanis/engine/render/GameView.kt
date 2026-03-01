b690e4c1ec338aa0874fc78602c12c74829c752c
Ninhoplayer6gg <filhofilho281@gmail.com>
2026-03-01 19:04:07 -0300

    Update GameView.kt

--- app/src/main/java/com/arkanis/engine/render/GameView.kt
+++ app/src/main/java/com/arkanis/engine/render/GameView.kt
@@ -1,1 +1,16 @@
+package com.arkanis.engine.render
 
+import android.content.Context
+import android.opengl.GLSurfaceView
+
+class GameView(context: Context) : GLSurfaceView(context) {
+
+    private val renderer: GameRenderer
+
+    init {
+        setEGLContextClientVersion(2) // OpenGL ES 2.0
+        renderer = GameRenderer()
+        setRenderer(renderer)
+        renderMode = RENDERMODE_CONTINUOUSLY
+    }
+}
