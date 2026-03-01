cf1aef4ca9b44d35db70933d857b6fff4cc259b8
Ninhoplayer6gg <filhofilho281@gmail.com>
2026-03-01 15:22:10 -0300

    Create SimpleRenderer.kt

---
+++ app/src/main/java/com/ninho/rpgxp/SimpleRenderer.kt
@@ -0,0 +1,21 @@
+package com.ninho.rpgxp
+
+import android.opengl.GLES30
+import android.opengl.GLSurfaceView
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+class SimpleRenderer : GLSurfaceView.Renderer {
+
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        GLES30.glClearColor(0.1f, 0.1f, 0.3f, 1.0f)
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        GLES30.glViewport(0, 0, width, height)
+    }
+
+    override fun onDrawFrame(gl: GL10?) {
+        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT)
+    }
+}
