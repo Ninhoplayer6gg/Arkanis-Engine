b2d910c5713def8006349717f84b580504533b21
Ninhoplayer6gg <filhofilho281@gmail.com>
2026-03-01 16:24:50 -0300

    Update MainActivity.kt

--- app/src/main/java/com/ninho/rpgxp/MainActivity.kt
+++ app/src/main/java/com/ninho/rpgxp/MainActivity.kt
@@ -8,22 +8,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var glView: GLSurfaceView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-        glView = GLSurfaceView(this)
-glView.setEGLContextClientVersion(2)
-// glView.setRenderer(SimpleRenderer())
+    try {
+
+        val glView = GLSurfaceView(this)
+        glView.setEGLContextClientVersion(2)
+        glView.setRenderer(SimpleRenderer())
+
         setContentView(glView)
-    }
 
-    override fun onPause() {
-        super.onPause()
-        glView.onPause()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        glView.onResume()
+    } catch (e: Exception) {
+        val textView = TextView(this)
+        textView.text = e.toString()
+        setContentView(textView)
     }
 }
