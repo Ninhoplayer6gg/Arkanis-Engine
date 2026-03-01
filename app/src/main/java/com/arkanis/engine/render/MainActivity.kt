b3b434aa796f710b3d81fb978dbb0d2cf744ce62
Ninhoplayer6gg <filhofilho281@gmail.com>
2026-03-01 19:08:20 -0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/arkanis/engine/render/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.arkanis.engine
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.arkanis.engine.render.GameView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val gameView = GameView(this)
+        setContentView(gameView)
+    }
+}
