6f77f81c193424225c0a61e3cffa2e544aeb46f9
Ninhoplayer6gg <filhofilho281@gmail.com>
2026-03-01 22:41:29 -0300

    Initialize Triangle object in GameRenderer

--- app/src/main/java/com/arkanis/engine/render/GameRenderer.kt
+++ app/src/main/java/com/arkanis/engine/render/GameRenderer.kt
@@ -2,13 +2,19 @@ package com.arkanis.engine.render
 
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
 class GameRenderer : GLSurfaceView.Renderer {
 
+    private lateinit var triangle: Triangle
+
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-        GLES20.glClearColor(0f, 0f, 0f, 1f) // fundo preto
+        GLES20.glClearColor(0f, 0f, 0f, 1f)
+        triangle = Triangle()
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
@@ -17,5 +23,6 @@ class GameRenderer : GLSurfaceView.Renderer {
 
     override fun onDrawFrame(gl: GL10?) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+        triangle.draw()
     }
 }
